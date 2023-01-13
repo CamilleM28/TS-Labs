203a2e957dacac58d5e935d90b42bbc83d63b6cf
CamilleM28 <Camillem2812@gmail.com>
2023-01-13 11:47:45 +0100

    reversed array

--- PuppyApp/ts-lab-puppiesReact/src/Components/PuppyList.tsx
+++ PuppyApp/ts-lab-puppiesReact/src/Components/PuppyList.tsx
@@ -3,36 +3,37 @@ import IPuppy from "../Types/IPuppy";
 import "../styles/PuppyList.css";
 
 interface IAllPuppies {
-  puppies: IPuppy[];
-  handleUpdate: Function;
-  setName: React.Dispatch<React.SetStateAction<string>>;
-  setBreed: React.Dispatch<React.SetStateAction<string>>;
-  setBirthDate: React.Dispatch<React.SetStateAction<string>>;
-  handleDelete: Function;
+    puppies: IPuppy[];
+    handleUpdate: Function;
+    setName: React.Dispatch<React.SetStateAction<string>>;
+    setBreed: React.Dispatch<React.SetStateAction<string>>;
+    setBirthDate: React.Dispatch<React.SetStateAction<string>>;
+    handleDelete: Function;
 }
+
 const PuppyList = ({
-  puppies,
-  handleUpdate,
-  setName,
-  setBreed,
-  setBirthDate,
-  handleDelete,
-}: IAllPuppies) => {
-  return (
-    <div className="PuppyList">
-      {puppies.reverse().map((puppy: IPuppy) => (
-        <Puppy
-          key={puppy.id}
-          puppy={puppy}
-          handleUpdate={handleUpdate}
-          setName={setName}
-          setBreed={setBreed}
-          setBirthDate={setBirthDate}
-          handleDelete={handleDelete}
-        />
-      ))}
-    </div>
-  );
+                       puppies,
+                       handleUpdate,
+                       setName,
+                       setBreed,
+                       setBirthDate,
+                       handleDelete,
+                   }: IAllPuppies) => {
+    return (
+        <div className="PuppyList">
+            {puppies.slice(0).reverse().map((puppy: IPuppy) => (
+                <Puppy
+                    key={puppy.id}
+                    puppy={puppy}
+                    handleUpdate={handleUpdate}
+                    setName={setName}
+                    setBreed={setBreed}
+                    setBirthDate={setBirthDate}
+                    handleDelete={handleDelete}
+                />
+            ))}
+        </div>
+    );
 };
 
 export default PuppyList;
